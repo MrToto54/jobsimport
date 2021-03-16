514d48315ded45fec7cfffeb75e1f84cd84dc0cd
Thomas Boda <boda.thomas@orange.fr>
2021-03-16 22:24:26 +0100

    Add the multiple files support

--- src/JobsImporter.php
+++ src/JobsImporter.php
@@ -1,9 +1,10 @@
 <?php
 
+use Models\Job;
+
 class JobsImporter
 {
     private $db;
-
     private $file;
 
     public function __construct($host, $username, $password, $databaseName, $file)
@@ -13,6 +14,7 @@ class JobsImporter
         /* connect to DB */
         try {
             $this->db = new PDO('mysql:host=' . $host . ';dbname=' . $databaseName, $username, $password);
+            $this->db->exec('DELETE FROM job');
         } catch (Exception $e) {
             die('DB error: ' . $e->getMessage() . "\n");
         }
@@ -20,24 +22,50 @@ class JobsImporter
 
     public function importJobs()
     {
-        /* remove existing items */
-        $this->db->exec('DELETE FROM job');
+        $xml = simplexml_load_file($this->file); /* parse XML file */
+        $count = 0; /* import each item */
+        $jobs = array();
 
-        /* parse XML file */
-        $xml = simplexml_load_file($this->file);
-
-        /* import each item */
-        $count = 0;
-        foreach ($xml->item as $item) {
-            $this->db->exec('INSERT INTO job (reference, title, description, url, company_name, publication) VALUES ('
-                . '\'' . addslashes($item->ref) . '\', '
-                . '\'' . addslashes($item->title) . '\', '
-                . '\'' . addslashes($item->description) . '\', '
-                . '\'' . addslashes($item->url) . '\', '
-                . '\'' . addslashes($item->company) . '\', '
-                . '\'' . addslashes($item->pubDate) . '\')'
-            );
-            $count++;
+        if (strpos($this->file, 'regionsjob.xml') !== false) {
+            foreach ($xml->item as $item) {
+                $jobs[] = new Job(
+                    addslashes($item->ref),
+                    addslashes($item->title),
+                    addslashes($item->description),
+                    addslashes($item->url),
+                    addslashes($item->company),
+                    addslashes($item->pubDate)
+                );
+            }
+        }
+        if (strpos($this->file, 'jobteaser.xml') !== false) {
+            foreach ($xml->offer as $item) {
+                $jobs[] = new Job(
+                    addslashes($item->reference),
+                    addslashes($item->title),
+                    addslashes($item->description),
+                    addslashes($item->link),
+                    addslashes($item->companyname),
+                    addslashes($item->publisheddate)
+                );
+            }
+        }
+        try {
+            $query = $this->db->prepare('INSERT INTO job (reference, title, description, url, company_name, publication) VALUES (?, ?, ?, ?, ?, ?)');
+			$this->db->beginTransaction();
+            foreach ($jobs as $job) {
+                $query->bindValue(1, $job->getRef());
+                $query->bindValue(2, $job->getTitle());
+                $query->bindValue(3, $job->getDesc());
+                $query->bindValue(4, $job->getLink());
+                $query->bindValue(5, $job->getCompany());
+                $query->bindValue(6, $job->getDate());
+                $query->execute();
+                $count++;
+            }
+            $this->db->commit();
+        } catch (Exception $e) {
+            die('Import ERROR: ' . $e->getMessage() . '\n');
         }
         return $count;
     }
--- src/app.php
+++ src/app.php
@@ -1,9 +1,11 @@
 <?php
 
 /************************************
-Entry point of the project.
-To be run from the command line.
-************************************/
+ Entry point of the project.
+ To be run from the command line.
+ ************************************/
+
+echo sprintf("Starting...\n");
 
 define('SQL_HOST', 'mariadb');
 define('SQL_USER', 'root');
@@ -12,29 +14,38 @@ define('SQL_DB', 'cmc_db');
 define('RESSOURCES_DIR', __DIR__ . '/../resources/');
 
 
-function __autoload(string $classname) {
+spl_autoload_register(function (string $classname) {
+    $classname = str_replace("\\", "/", $classname);
     include_once(__DIR__ . '/' . $classname . '.php');
+});
+
+class App {
+    public function import() { /* import jobs from all .xml files */
+        $files = scandir(RESSOURCES_DIR);
+        foreach($files as $file) {
+            if (strpos($file, '.xml') !== false) {
+                $jobsImporter = new JobsImporter(SQL_HOST, SQL_USER, SQL_PWD, SQL_DB, RESSOURCES_DIR . $file);
+                $count = $jobsImporter->importJobs();
+            }
+        }
+        
+        echo sprintf("> %d jobs imported.\n", $count);
+    }
+    
+    public function listJobs() { /* list jobs */
+        $jobsLister = new JobsLister(SQL_HOST, SQL_USER, SQL_PWD, SQL_DB);
+        $jobs = $jobsLister->listJobs();
+        
+        echo sprintf("> all jobs (%d):\n", count($jobs));
+        foreach ($jobs as $job) {
+            echo sprintf(" %d: %s - %s - %s\n", $job['id'], $job['reference'], $job['title'], $job['publication']);
+        }
+    }
 }
 
+$app = new App();
 
-echo sprintf("Starting...\n");
-
-
-/* import jobs from regionsjob.xml */
-$jobsImporter = new JobsImporter(SQL_HOST, SQL_USER, SQL_PWD, SQL_DB, RESSOURCES_DIR . 'regionsjob.xml');
-$count = $jobsImporter->importJobs();
-
-echo sprintf("> %d jobs imported.\n", $count);
-
-
-/* list jobs */
-$jobsLister = new JobsLister(SQL_HOST, SQL_USER, SQL_PWD, SQL_DB);
-$jobs = $jobsLister->listJobs();
-
-echo sprintf("> all jobs (%d):\n", count($jobs));
-foreach ($jobs as $job) {
-    echo sprintf(" %d: %s - %s - %s\n", $job['id'], $job['reference'], $job['title'], $job['publication']);
-}
-
+$app->import();
+$app->listJobs();
 
 echo sprintf("Done.\n");
